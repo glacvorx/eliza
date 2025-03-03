deca882c7c6078031f057da2b2946b45e2396dba
glacvorx <rongheng_1@hotmail.com>
2025-03-03 22:52:54 +0800

    Added new line to trigger new build.

--- packages/client-twitter/src/post.ts
+++ packages/client-twitter/src/post.ts
@@ -253,6 +253,7 @@ export class TwitterPostClient {
             // Add a 5 minute delay for GAME SDK start up so posts get spaced out with Eliza start up tweets
             await new Promise(r => setTimeout(r, 5 * 60 * 1000));
 
+
             const runVirtualsGAMELoop = async () => {
                 let result: { success: boolean; error?: string; };
                 do {
