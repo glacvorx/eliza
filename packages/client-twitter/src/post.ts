797a6f4ec627339049f0f4ce3df87f2f650bcfd0
glacvorx <rongheng_1@hotmail.com>
2025-03-03 17:50:15 +0800

    Added delay to GAME SDK start up to to space out duration with Eliza start up tweets.

--- packages/client-twitter/src/post.ts
+++ packages/client-twitter/src/post.ts
@@ -250,6 +250,9 @@ export class TwitterPostClient {
 
         // Initialize Virtuals GAME if configured
         if (this.client.twitterConfig.VIRTUALS_GAME_SDK_API_KEY) {
+            // Add a 5 minute delay for GAME SDK start up so posts get spaced out with Eliza start up tweets
+            await new Promise(r => setTimeout(r, 5 * 60 * 1000));
+
             const runVirtualsGAMELoop = async () => {
                 let result: { success: boolean; error?: string; };
                 do {
